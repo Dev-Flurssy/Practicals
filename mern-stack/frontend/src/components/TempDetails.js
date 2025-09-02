363c8d49b2bcaf0b8f9cf04950bb409e69e4c90e
Dev-Flurssy <alayandebisola0@gmail.com>
2025-09-02 18:18:45 +0100

    making minor updates

--- mern-stack/frontend/src/components/TempDetails.js
+++ mern-stack/frontend/src/components/TempDetails.js
@@ -1,10 +1,44 @@
-const TempDetails = ({workout}) => {
-    return ( <div className="workout-details">
-        <h4>{workout.title}</h4>
-        <p><strong>Load (kg): </strong>{workout.Load}</p>
-        <p><strong>Reps: </strong>{workout.Reps}</p>
-        <p>{workout.createdAt}</p>
-    </div> );
-}
- 
+import { useWorkoutContext } from "../hooks/useWorkoutContext.js";
+import formatDistanceToNow from "date-fns/formatDistanceToNow";
+
+const TempDetails = ({ workout }) => {
+  const { dispatch } = useWorkoutContext();
+
+  const handleDelete = async () => {
+    const response = await fetch("/api/workouts/" + workout._id, {
+      method: "DELETE",
+    });
+    const json = await response.json();
+
+    if (response.ok) {
+      dispatch({ type: "DELETE_WORKOUT", payload: json });
+    }
+  };
+
+  return (
+    <div className="workout-details">
+      <h4>{workout.title}</h4>
+      <p>
+        <strong>Load (kg): </strong>
+        {workout.load}
+      </p>
+      <p>
+        <strong>Reps: </strong>
+        {workout.reps}
+      </p>
+      <p>
+        {formatDistanceToNow(new Date(workout.createdAt), { addSuffix: true })}
+      </p>
+
+      <span
+        className="material-symbols-outlined"
+        onClick={handleDelete}
+        style={{ cursor: "pointer", color: "red" }}
+      >
+        delete
+      </span>
+    </div>
+  );
+};
+
 export default TempDetails;
--- mern-stack/frontend/src/pages/Home.js
+++ mern-stack/frontend/src/pages/Home.js
@@ -1,21 +1,22 @@
-import { useEffect, useState } from "react";
+import { useEffect, } from "react";
 import TempDetails from "../components/TempDetails.js";
 import WorkoutForm from "../components/WorkoutForm.js";
+import { useWorkoutContext } from "../hooks/useWorkoutContext.js";
 
 
 const Home = () => {
-const [workouts, setWorkouts] = useState(null);
+const{workouts, dispatch} = useWorkoutContext();
 
 useEffect(() =>{
     const fetchWorkouts = async () =>{
         const response = await fetch('/api/workouts');
         const jsonRes = await response.json();
         if(response.ok){
-            setWorkouts(jsonRes);
+            dispatch({type: 'SET_WORKOUTS', payload: jsonRes})
         }
     }
     fetchWorkouts();
-}, []);
+}, [dispatch]);
 
     return ( <div className="home">
         <div className="workouts">
